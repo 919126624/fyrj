e32ff0a30428f0fffc432559b2824a6772ffd4a8
919126624@qq.com <919126624@qq.com>
2016-11-29 17:43:37 +0800

    SALL

--- src/main/java/com/fyrj/gitDemo/App.java
+++ src/main/java/com/fyrj/gitDemo/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "S1" );
+		System.out.println( "L1" );
         System.out.println( "S2" );
     }
 }
