81eb604acecc897cbb8d11616c2575d2eb826089
919126624 <919126624@qq.com>
2016-11-29 17:16:44 +0800

    Update App.java
    333服务端提交！

--- src/main/java/com/fyrj/gitDemo/App.java
+++ src/main/java/com/fyrj/gitDemo/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World==service222!" );
+         System.out.println( "Hello World==service333!" );
     }
 }
