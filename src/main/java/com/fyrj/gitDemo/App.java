5b37240f902604e16ede5539b10c5b0ed021996e
919126624 <919126624@qq.com>
2016-11-29 17:30:29 +0800

    Update App.java

--- src/main/java/com/fyrj/gitDemo/App.java
+++ src/main/java/com/fyrj/gitDemo/App.java
@@ -8,12 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World==service222!" );
-         System.out.println( "Hello World==service333!" );
-		  System.out.println( "Hello World==local111!" )
- System.out.println( "Hello World==service444!" )
-  System.out.println( "Hello World==local222!" )
-    System.out.println( "Hello World==local333!" )
+        System.out.println( "S1" );
     }
 }
