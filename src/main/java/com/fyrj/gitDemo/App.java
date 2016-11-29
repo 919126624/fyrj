cb74d29cc7119d40b067d3c766179207e7a3c820
919126624@qq.com <919126624@qq.com>
2016-11-29 17:16:40 +0800

    local111提交

--- src/main/java/com/fyrj/gitDemo/App.java
+++ src/main/java/com/fyrj/gitDemo/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World==service222!" );
          System.out.println( "Hello World==service333!" );
+		  System.out.println( "Hello World==local111!" )
     }
 }
