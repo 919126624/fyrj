7cc0772670d0acb5da1d6a5197c9b60f3a3f7625
919126624@qq.com <919126624@qq.com>
2016-11-29 16:20:01 +0800

    修改逻辑1

--- src/main/java/com/fyrj/gitDemo/App.java
+++ src/main/java/com/fyrj/gitDemo/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!aaa,再次测试" );
-        System.out.println("我是bug2.0.1分支代码");
-        System.out.println("tag01代码。。。");
+        System.out.println( "Hello World!" );
     }
 }
